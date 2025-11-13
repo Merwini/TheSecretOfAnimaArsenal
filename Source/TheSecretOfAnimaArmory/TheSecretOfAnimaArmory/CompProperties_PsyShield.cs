fdf42ec7d91e787cfce9f54c31385f05bb43e9e5
Merwini <86023141+Merwini@users.noreply.github.com>
2025-11-13 15:57:35 -0500

    Initial psy shield implementation

---
+++ Source/TheSecretOfAnimaArmory/TheSecretOfAnimaArmory/CompProperties_PsyShield.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace nuff.tsoa.arsenal
+{
+    public class CompProperties_PsyShield : CompProperties
+    {
+        public float heatPerDamage = 1f;
+
+        public float resetHeatPercent = 0.8f;
+
+        public int resetDelayTicks = 120;
+
+        public float minDrawSize = 1.2f;
+
+        public float maxDrawSize = 1.55f;
+
+        public CompProperties_PsyShield()
+        {
+            this.compClass = typeof(CompPsyShield);
+        }
+    }
+}
