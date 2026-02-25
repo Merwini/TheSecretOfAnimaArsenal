b8c259e9b4f7819f34ee8058d521d33ca03b7da7
Nuff <me.megalomatt@gmail.com>
2026-02-25 16:40:47 -0500

    Removed some debug logging Cleaned up references

--- Source/TheSecretOfAnimaArsenal/Workers/Recipe_ExtractVirion.cs
+++ Source/TheSecretOfAnimaArsenal/Workers/Recipe_ExtractVirion.cs
@@ -12,23 +12,18 @@ public class Recipe_ExtractVirion : Recipe_Surgery
 {
     public override bool AvailableOnNow(Thing thing, BodyPartRecord part = null)
     {
-        Log.Message("checking if recipe_extractvirior is available on " + thing.LabelShort);
         if (!base.AvailableOnNow(thing, part))
         {
-            Log.Message("base not available now");
             return false;
         }
 
         if (thing is not Pawn pawn)
         {
-            Log.Message("not a pawn");
             return false;
 
         }
 
-        bool available = pawn.health?.hediffSet?.GetFirstHediffOfDef(TSOAA_DefOf.TSOA_VirionHediff) != null;
-        Log.Message("found Virion Hediff: " + available);
-        return available;
+        return pawn.health?.hediffSet?.GetFirstHediffOfDef(TSOAA_DefOf.TSOA_VirionHediff) != null;
     }
 
     public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
